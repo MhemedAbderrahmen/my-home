96e263ce983a63223b9264eb68948ad67fdf2802
Abderrahmen Mhemed <mhemedabderrahmen@gmail.com>
2024-08-02 17:48:14 +0200

    fix

--- src/app/inventory/page.tsx
+++ src/app/inventory/page.tsx
@@ -1,12 +1,15 @@
 import InventoryList from "~/components/inventory/inventory-list";
+import { HydrateClient } from "~/trpc/server";
 
 export default function Inventory() {
   return (
-    <main className="flex flex-col gap-4 p-4">
-      <div className="flex w-full flex-row items-center justify-between">
-        📦 House Inventory
-      </div>
-      <InventoryList />
-    </main>
+    <HydrateClient>
+      <main className="flex flex-col gap-4 p-4">
+        <div className="flex w-full flex-row items-center justify-between">
+          📦 House Inventory
+        </div>
+        <InventoryList />
+      </main>
+    </HydrateClient>
   );
 }
