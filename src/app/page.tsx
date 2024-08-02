d4342d3421c639b1ba1a30b0e9691daa168015bd
AbderrahmenMhemed <abderrahmen.mhemed@wevioo.com>
2024-08-02 11:35:51 +0100

    shopping lists

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,26 @@
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
+
 export default function Home() {
   return (
     <main
       className="flex flex-col items-center justify-between p-4"
       suppressHydrationWarning
     >
-      Hello
+      <Card>
+        <CardHeader>
+          <CardTitle>Welcome! 👋</CardTitle>
+          <CardDescription>
+            Welcome to homely, an app to help you manage your home.
+          </CardDescription>
+        </CardHeader>
+      </Card>
+      <br />
+      Overview
     </main>
   );
 }
