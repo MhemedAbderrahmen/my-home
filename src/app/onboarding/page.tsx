c8cf6ac3879695c8c6eeecc1098106f33d90a579
Abderrahmen Mhemed <mhemedabderrahmen@gmail.com>
2024-08-08 09:04:38 +0200

    fix

---
+++ src/app/onboarding/page.tsx
@@ -0,0 +1,10 @@
+export default function Onboarding() {
+  return (
+    <main
+      className="flex flex-col items-center justify-between p-4"
+      suppressHydrationWarning
+    >
+      <section className="w-full md:max-w-screen-sm">Onboarding page</section>
+    </main>
+  );
+}
