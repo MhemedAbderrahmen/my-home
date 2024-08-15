5f43fd268d54ce31ab6e2fd1f0007f3f278c69c1
AbderrahmenMhemed <abderrahmen.mhemed@wevioo.com>
2024-08-15 16:32:15 +0100

    fixing notification list

--- src/components/notifications/notifications-list.tsx
+++ src/components/notifications/notifications-list.tsx
@@ -5,10 +5,29 @@ import { api } from "~/trpc/react";
 import { Button } from "../ui/button";
 import { Card, CardContent, CardHeader } from "../ui/card";
 
-function InviteNotification() {
-  return <div></div>;
+function InviteNotification({
+  username,
+  createdAt,
+}: {
+  username: string;
+  createdAt: Date;
+}) {
+  return (
+    <div className="flex flex-col gap-2">
+      <div>You have recieved an partner invite from {username} </div>
+      <small className="text-muted-foreground">
+        {dayjs(createdAt).format(DEFAULT_DATE_FORMAT)}
+      </small>
+      <div className="flex items-center justify-between gap-2">
+        <Button className="w-full" variant={"destructive"}>
+          Decline
+        </Button>
+        <Button className="w-full">Accept</Button>
+      </div>
+    </div>
+  );
 }
-// oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/amro.omp.json' | Invoke-Expression
+
 export function NotificationsList() {
   const { data } = api.notifications.get.useQuery();
   return (
@@ -17,22 +36,13 @@ export function NotificationsList() {
         <CardHeader>You have ( {data?.length} ) notifications</CardHeader>
         <CardContent>
           {data?.map((notification, index) => (
-            <div key={index} className="flex flex-col gap-2">
+            <div key={index}>
               {notification.type === "INVITE" && (
-                <div>
-                  You have recieved an partner invite from{" "}
-                  {notification.sender.username}{" "}
-                </div>
+                <InviteNotification
+                  username={notification.sender.username}
+                  createdAt={notification.createdAt}
+                />
               )}
-              <small className="text-muted-foreground">
-                {dayjs(notification.createdAt).format(DEFAULT_DATE_FORMAT)}
-              </small>
-              <div className="flex items-center justify-between gap-2">
-                <Button className="w-full" variant={"destructive"}>
-                  Decline
-                </Button>
-                <Button className="w-full">Accept</Button>
-              </div>
             </div>
           ))}
         </CardContent>
